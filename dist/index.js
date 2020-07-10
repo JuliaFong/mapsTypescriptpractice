da205f1e9236c2d2be0faeb5a073fff7bd0d6daa
Julia Fong <Juliamzfong@gmail.com>
2020-07-10 16:46:30 -0500

    Add google  user marker

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64320" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60295" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 // import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+
+const user = new User()
+const customMap = new CustomMap("map") 
+
+customMap.addUserMarker(user)
 
 
 
-new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+
+
